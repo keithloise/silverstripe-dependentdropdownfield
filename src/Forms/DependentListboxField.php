02bd13366eb5f5dc4e56bda4a20c3b92e8482cc2
keithloise <k8t.loise16@gmail.com>
2023-06-30 01:17:55 +1200

    added DependentListboxField

---
+++ src/Forms/DependentListboxField.php
@@ -0,0 +1,187 @@
+<?php
+
+namespace Sheadawson\DependentDropdown\Forms;
+
+use SilverStripe\Admin\LeftAndMain;
+use SilverStripe\Control\Controller;
+use SilverStripe\Forms\DropdownField;
+use SilverStripe\Forms\ListboxField;
+use SilverStripe\Control\HTTPResponse;
+use SilverStripe\Core\Convert;
+use SilverStripe\ORM\Map;
+use SilverStripe\View\Requirements;
+use SilverStripe\Forms\FormField;
+
+/**
+ * Class DependentDropdownField
+ *
+ * A dropdown that depends on another dropdown for populating values, and calls
+ * a callback when that dropdown is updated.
+ *
+ * @package SilverStripe\Forms
+ */
+class DependentListboxField extends ListboxField
+{
+    /**
+     * @var array
+     */
+    private static $allowed_actions = [
+        'load',
+    ];
+
+    /**
+     * @var
+     */
+    protected $depends;
+
+    /**
+     * @var
+     */
+    protected $unselected;
+
+    /**
+     * @var \Closure
+     */
+    protected $sourceCallback;
+
+    /**
+     * DependentDropdownField constructor.
+     * @param string $name
+     * @param string $title
+     * @param \Closure $source
+     * @param string $value
+     * @param $form
+     * @param string $emptyString
+     */
+    public function __construct($name, $title = null, \Closure $source = null, $value = '', $form = null, $emptyString = null)
+    {
+        parent::__construct($name, $title, [], $value, $form, $emptyString);
+
+        // we are unable to store Closure as a normal source
+        $this->sourceCallback = $source;
+        $this
+            ->addExtraClass('dependent-listbox')
+            ->addExtraClass('listbox');
+    }
+
+    /**
+     * @param $request
+     * @return HTTPResponse
+     */
+    public function load($request)
+    {
+        $response = new HTTPResponse();
+        $response->addHeader('Content-Type', 'application/json');
+
+        $items = call_user_func($this->sourceCallback, $request->getVar('val'));
+        $results = [];
+        if ($items) {
+            foreach ($items as $k => $v) {
+                $results[] = ['k' => $k, 'v' => $v];
+            }
+        }
+
+        $response->setBody(Convert::array2json($results));
+
+        return $response;
+    }
+
+    /**
+     * @return mixed
+     */
+    public function getDepends()
+    {
+        return $this->depends;
+    }
+
+    /**
+     * @param FormField $field
+     * @return $this
+     */
+    public function setDepends(FormField $field)
+    {
+        $this->depends = $field;
+
+        return $this;
+    }
+
+    /**
+     * @return mixed
+     */
+    public function getUnselectedString()
+    {
+        return $this->unselected;
+    }
+
+    /**
+     * @param $string
+     * @return $this
+     */
+    public function setUnselectedString($string)
+    {
+        $this->unselected = $string;
+
+        return $this;
+    }
+
+    /**
+     * @return array|\ArrayAccess|mixed
+     */
+    public function getSource()
+    {
+        $val = $this->depends->Value();
+
+        if (
+            !$val
+            && method_exists($this->depends, 'getHasEmptyDefault')
+            && !$this->depends->getHasEmptyDefault()
+        ) {
+            $dependsSource = array_keys($this->depends->getSource());
+            $val = isset($dependsSource[0]) ? $dependsSource[0] : null;
+        }
+
+        if (!$val) {
+            $source = [];
+        } else {
+            $source = call_user_func($this->sourceCallback, $val);
+            if ($source instanceof Map) {
+                $source = $source->toArray();
+            }
+        }
+
+        return $source;
+    }
+
+    /**
+     * @param \Closure $source
+     * @return $this
+     */
+    public function setSource($source)
+    {
+        $this->sourceCallback = $source;
+        return $this;
+    }
+
+    /**
+     * @param array $properties
+     * @return string
+     */
+    public function Field($properties = [])
+    {
+        if (!is_subclass_of(Controller::curr(), LeftAndMain::class)) {
+            Requirements::javascript('silverstripe/admin:thirdparty/jquery-entwine/dist/jquery.entwine-dist.js');
+        }
+
+        Requirements::javascript(
+            'sheadawson/silverstripe-dependentdropdownfield:client/js/dependentlistboxfield.js'
+        );
+
+        $this->addExtraClass('dependent-listbox');
+        $this->addExtraClass('listbox');
+        $this->setAttribute('data-link', $this->Link('load'));
+        $this->setAttribute('data-depends', $this->getDepends()->getName());
+        $this->setAttribute('data-unselected', $this->getUnselectedString());
+
+        return parent::Field($properties);
+    }
+}
